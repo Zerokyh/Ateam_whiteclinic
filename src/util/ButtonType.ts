5250d6ff9390e646ab466fb5b8030edb86c9ca62
Zerokyh <koreaguh@naver.com>
2024-09-01 19:24:45 +0900

    9/1 김윤호 - 충돌파일 업데이트

--- src/util/ButtonType.ts
+++ src/util/ButtonType.ts
@@ -1,6 +1,6 @@
-import { ButtonStyle } from '@/app/hwanhee/style/ButtonStyle';
+import { ButtonStyle } from '@/styles/ButtonStyle';
 
-export type CustomButtonProps = {
+export type ACustomButtonProps = {
   variant: 'text' | 'contained' | 'outlined';
   text: '등록중지' | '취소' | '등록' | '아니오' | '추가등록' | '급여사항확인';
   onClick?: () => void;
