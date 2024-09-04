9aac67b81a64d8f1729b218074cf217204e37d0e
Zerokyh <koreaguh@naver.com>
2024-09-04 11:15:43 +0900

    9/4 김윤호 - 주문등록 페이지 업데이트

--- src/app/sales/monthly/page.tsx
+++ src/app/sales/monthly/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-  return <div>월간 매출 보기</div>;
-}
+'use client';
+
+import AMonthPlan from '@/conponents/organism/Select/AMonthPlan';
+import { Box } from '@mui/material';
+
+const Page = () => {
+  return (
+    <Box
+      sx={{
+        height: 'calc(100vh - 112px)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box
+              sx={{
+                width: 'calc(100vw - 340px)',
+                height: 'calc(100vh - 112px)',
+                bgcolor: '#f2f2f2',
+              }}
+            >
+              <Box>
+                <AMonthPlan />
+              </Box>
+              <Box></Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Page;
