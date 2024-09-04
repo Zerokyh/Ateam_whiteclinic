730908f75232ccd6744d16b8b222494fa41db12a
Zerokyh <koreaguh@naver.com>
2024-09-04 21:24:49 +0900

    9/4 김윤호 - 일정 페이지 업데이트

--- src/constants/MonthsPlan.ts
+++ src/constants/MonthsPlan.ts
@@ -3,17 +3,3 @@ const generateMonths = () => {
 };
 
 export const MonthsPlan: string[] = generateMonths();
-// export const Months = [
-//   '1월',
-//   '2월',
-//   '3월',
-//   '4월',
-//   '5월',
-//   '6월',
-//   '7월',
-//   '8월',
-//   '9월',
-//   '10월',
-//   '11월',
-//   '12월',
-// ];
