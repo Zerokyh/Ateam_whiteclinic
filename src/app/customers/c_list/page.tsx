181870c659dc763dcbed4d382bffdade0b58e779
Zerokyh <koreaguh@naver.com>
2024-09-10 14:55:33 +0900

    9/10 김윤호 - 업데이트

--- src/app/customers/c_list/page.tsx
+++ src/app/customers/c_list/page.tsx
@@ -1,20 +1,8 @@
 import SalesInfo from '@/conponents/organism/Sales/SalesInfo';
-import { Box } from '@mui/material';
+import CenteredLayout from './CenteredLayout';
 
 const Page = () => {
-  return (
-    <Box
-      sx={{
-        height: 'calc(100vh - 112px)',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <SalesInfo />
-    </Box>
-  );
+  return <CenteredLayout children={<SalesInfo />} />;
 };
 
 export default Page;
